13814e1c7bebd481ee787ac59639a888881df7ac
Rigmanisten <einar.gurell@gmail.com>
2025-05-06 11:38:46 +0200

    added loadRecommendationGraph

--- src/main/java/se/su/ovning4/Exercise4.java
+++ src/main/java/se/su/ovning4/Exercise4.java
@@ -1,5 +1,8 @@
 package se.su.ovning4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.SortedMap;
@@ -8,6 +11,7 @@ public class Exercise4 {
     private Graph<Node> graph = new ListGraph<>();
 
     public void loadLocationGraph(String fileName){
+
     }
 
     public SortedMap<Integer, SortedSet<Record>> getAlsoLiked(Record item) {
@@ -23,6 +27,20 @@ public class Exercise4 {
     }
 
     public void loadRecommendationGraph(String fileName) {
-    }
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(";");
+                Person person = new Person(parts[0]);
+                Record record = new Record(parts[1], parts[2]);
 
+                graph.add(person);
+                graph.add(record);
+                graph.connect(person, record, "rekommendation", 0);  // eller rätt vikt
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
