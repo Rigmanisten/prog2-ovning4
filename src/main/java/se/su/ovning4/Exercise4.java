b7e1cf6943e3bbcbc965a4ca282dd4334be90644
linus <linus00552@gmail.com>
2025-05-06 11:13:09 +0200

    Imoprt for Reding in Ex4

--- src/main/java/se/su/ovning4/Exercise4.java
+++ src/main/java/se/su/ovning4/Exercise4.java
@@ -1,13 +1,26 @@
 package se.su.ovning4;
 
+
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.SortedMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Exercise4 {
     private Graph<Node> graph = new ListGraph<>();
 
     public void loadLocationGraph(String fileName){
+      try{
+
+      }catch (FileNotFoundException e){
+         System.out.printf("%s not found",fileName);
+      }
     }
 
     public SortedMap<Integer, SortedSet<Record>> getAlsoLiked(Record item) {
